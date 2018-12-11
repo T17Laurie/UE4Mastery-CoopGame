ca4c230581a0e1d1edbf0752e8330f0c184a7138
Laurie Dobson <42836383+T17Laurie@users.noreply.github.com>
2018-12-11 10:01:56 +0000

    68. Shake Camera While Firing

--- Source/CoopGame/Public/SWeapon.h
+++ Source/CoopGame/Public/SWeapon.h
@@ -45,6 +45,7 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 	UParticleSystem*  TracerEffect;
 
-	
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	TSubclassOf<UCameraShake> FireCamShake;
 	
 };
